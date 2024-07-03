b943a2c72af97f9469abd71f68f2fdd5bd274ac1
wlangera <ward.langeraert@inbo.be>
2024-07-03 16:51:33 +0200

    use logical naming bootstrapping theory

--- source/R/bootstrap_list_to_df.R
+++ source/R/bootstrap_list_to_df.R
@@ -1,25 +1,25 @@
 bootstrap_list_to_df <- function(bootstrap_list) {
   bootstrap_data_df <- sapply(bootstrap_list, function(df) df$t) %>%
     as.data.frame() %>%
-    tibble::rownames_to_column(var = "rep") %>%
-    tidyr::pivot_longer(cols = -rep,
-                        values_to = "original",
+    tibble::rownames_to_column(var = "sample") %>%
+    tidyr::pivot_longer(cols = -sample,
+                        values_to = "est_boot",
                         names_to = "year") %>%
-    dplyr::mutate(rep = as.numeric(rep))
+    dplyr::mutate(sample = as.numeric(sample))
 
   bootstrap_summaries <- data.frame(
     year = names(bootstrap_list),
-    stat_value = sapply(bootstrap_list, function(df) df$t0),
-    stat_se = sapply(bootstrap_list, function(df) stats::sd(df$t))
+    est_original = sapply(bootstrap_list, function(df) df$t0),
+    se_boot = sapply(bootstrap_list, function(df) stats::sd(df$t))
   )
 
   bootstrap_data_full <- bootstrap_data_df %>%
     dplyr::full_join(bootstrap_summaries, by = dplyr::join_by("year")) %>%
-    dplyr::arrange(rep, year) %>%
+    dplyr::arrange(sample, year) %>%
     dplyr::rowwise() %>%
-    dplyr::mutate(diff = .data$original - .data$stat_value) %>%
+    dplyr::mutate(diff = .data$est_boot - .data$est_original) %>%
     dplyr::group_by(year) %>%
-    dplyr::mutate(bias = mean(.data$diff)) %>%
+    dplyr::mutate(bias_boot = mean(.data$diff)) %>%
     dplyr::ungroup() %>%
     dplyr::select(-"diff")
 
