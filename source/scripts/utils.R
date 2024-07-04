16b00bb47cac611a15969f84eba6e27054b3c728
wlangera <ward.langeraert@inbo.be>
2024-07-04 15:53:15 +0200

    create function for grouped regression

--- source/scripts/utils.R
+++ source/scripts/utils.R
@@ -9,3 +9,11 @@ logit <- function(p) {
 inv_logit <- function(l) {
   exp(l) / (1 + exp(l))
 }
+
+# Perform regression on a grouped dataframe and export summary statistics
+grouped_regression <- function(df, formula) {
+  summary(stats::lm(formula, data = df))$coefficients %>%
+    data.frame() %>%
+    tibble::rownames_to_column("param") %>%
+    dplyr::mutate("P < 0.05" = .data$Pr...t.. < 0.05)
+}
