1fe52fe92532529474e938687f050f926dc72549
wlangera <ward.langeraert@inbo.be>
2024-07-08 12:01:37 +0200

    add reference to function

--- source/scripts/perform_bootstrap_ts.R
+++ source/scripts/perform_bootstrap_ts.R
@@ -8,6 +8,8 @@
 #' @param fun A function which when applied to data returns the statistic(s) of
 #' interest.
 #' @param samples The number of bootstrap replicates. A single positive integer.
+#' @param ref_group A string indicating the reference year to compare the
+#' statistic. Default `NA`, no reference year is used.
 #' @param seed The seed for random number generation to make results
 #' reproducible.
 #'
@@ -18,25 +20,30 @@ perform_bootstrap_ts <- function(
     data_cube_df,
     fun,
     samples = 1000,
+    ref_group = NA,
     seed = 123) {
   require("dplyr")
   require("rlang")
 
   withr::local_seed(seed)
 
-  bootstrap_list <- data_cube_df %>%
-    dplyr::summarize(num_occ = sum(.data$obs),
-                     .by = c("year", "taxonKey")) %>%
-    dplyr::arrange(.data$year) %>%
-    tidyr::pivot_wider(names_from = "year",
-                       values_from = "num_occ",
-                       values_fill = 0) %>%
-    tibble::column_to_rownames("taxonKey") %>%
-    as.list() %>%
-    purrr::map(~boot::boot(data = .,
-                           statistic = boot_statistic,
-                           R = samples,
-                           fun = fun))
+  if (is.na(ref_group)) {
+    bootstrap_list <- data_cube_df %>%
+      dplyr::summarize(num_occ = sum(.data$obs),
+                       .by = c("year", "taxonKey")) %>%
+      dplyr::arrange(.data$year) %>%
+      tidyr::pivot_wider(names_from = "year",
+                         values_from = "num_occ",
+                         values_fill = 0) %>%
+      tibble::column_to_rownames("taxonKey") %>%
+      as.list() %>%
+      purrr::map(~boot::boot(data = .,
+                             statistic = boot_statistic,
+                             R = samples,
+                             fun = fun))
+  } else {
+
+  }
 
   return(bootstrap_list)
 }
