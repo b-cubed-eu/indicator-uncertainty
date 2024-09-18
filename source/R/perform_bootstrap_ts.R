25e5348155e95ff53baf7c665941049c8ab88fcd
Ward Langeraert <ward.langeraert@inbo.be>
2024-09-18 11:58:50 +0200

    flexible temporal column name argument

--- source/R/perform_bootstrap_ts.R
+++ source/R/perform_bootstrap_ts.R
@@ -1,26 +1,29 @@
-#' Perform bootstrapping per year for a calculated statistic
+#' Perform bootstrapping for a calculated statistic over time
 #'
 #' This function generate `samples` bootstrap replicates of a statistic applied
-#' to a data cube per year.
+#' to a data cube per time point (e.g., year, month ...).
 #'
 #' @param data_cube_df A dataframe containing data in biodiversity data cube
 #' format.
 #' @param fun A function which when applied to data returns the statistic(s) of
 #' interest.
 #' @param samples The number of bootstrap replicates. A single positive integer.
-#' @param ref_group A string indicating the reference year to compare the
-#' statistic. Default `NA`, no reference year is used.
+#' @param ref_group A string indicating the reference time point to compare the
+#' statistic. Default `NA`, no reference time point is used.
+#' @param temporal_col_name The temporal column name of `data_cube_df`
+#' (e.g., year, month ...) containing time point values. Default `year`.
 #' @param seed The seed for random number generation to make results
 #' reproducible.
 #'
-#' @returns The returned value is a list of objects of class `"boot"` per year.
-#' See `boot::boot()`.
+#' @returns The returned value is a list of objects of class `"boot"` per time
+#' point. See `boot::boot()`.
 
 perform_bootstrap_ts <- function(
     data_cube_df,
     fun,
     samples = 1000,
     ref_group = NA,
+    temporal_col_name = "year",
     seed = 123) {
   require("dplyr")
   require("rlang")
@@ -28,12 +31,12 @@ perform_bootstrap_ts <- function(
   withr::local_seed(seed)
 
   if (is.na(ref_group)) {
-    # Summarise data by year
+    # Summarise data by temporal column
     bootstrap_list <- data_cube_df %>%
       dplyr::summarize(num_occ = sum(.data$obs),
-                       .by = c("year", "taxonKey")) %>%
-      dplyr::arrange(.data$year) %>%
-      tidyr::pivot_wider(names_from = "year",
+                       .by = all_of(c(temporal_col_name, "taxonKey"))) %>%
+      dplyr::arrange(.data[[temporal_col_name]]) %>%
+      tidyr::pivot_wider(names_from = temporal_col_name,
                          values_from = "num_occ",
                          values_fill = 0) %>%
       tibble::column_to_rownames("taxonKey") %>%
@@ -45,12 +48,12 @@ perform_bootstrap_ts <- function(
         R = samples,
         fun = fun))
   } else {
-    # Summarise data by year
+    # Summarise data by temporal column
     sum_data_list <- data_cube_df %>%
       dplyr::summarize(num_occ = sum(.data$obs),
-                       .by = c("year", "taxonKey")) %>%
-      dplyr::arrange(.data$year) %>%
-      tidyr::pivot_wider(names_from = "year",
+                       .by = all_of(c(temporal_col_name, "taxonKey"))) %>%
+      dplyr::arrange(.data[[temporal_col_name]]) %>%
+      tidyr::pivot_wider(names_from = temporal_col_name,
                          values_from = "num_occ",
                          values_fill = 0) %>%
       tibble::column_to_rownames("taxonKey") %>%
