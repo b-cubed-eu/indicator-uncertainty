f26c6702becea4412418aaa1c3b3372d4e2935db
Ward Langeraert <ward.langeraert@inbo.be>
2024-10-24 17:21:42 +0200

    add code for loo species function

---
+++ source/R/leave_one_species_out_ts.R
@@ -0,0 +1,62 @@
+#' Perform bootstrapping for a calculated statistic over time
+#'
+#' This function generate `samples` bootstrap replicates of a statistic applied
+#' to a data cube per time point (e.g., year, month ...).
+#'
+#' @param data_cube_df A dataframe containing data in biodiversity data cube
+#' format. See `b3gbi::process_cube()`.
+#' @param fun A function which when applied to data returns the statistic(s) of
+#' interest.
+#' @param samples The number of bootstrap replicates. A single positive integer.
+#' @param ref_group A string indicating the reference time point to compare the
+#' statistic. Default `NA`, no reference time point is used.
+#' @param temporal_col_name The temporal column name of `data_cube_df`
+#' (e.g., year, month ...) containing time point values. Default `year`.
+#' @param seed The seed for random number generation to make results
+#' reproducible.
+#'
+#' @returns The returned value is a list of objects of class `"boot"` per time
+#' point. See `boot::boot()`.
+
+leave_one_species_out_ts <- function(
+    data_cube_df,
+    fun,
+    temporal_col_name = "year",
+    ...,
+    seed = 123) {
+  require("dplyr")
+  require("rlang")
+
+  # Calculate indicator based on all data
+  true_indicator <- fun(data_cube_df)$data
+
+  # Get species list
+  species_list <- unique(data_cube_df$data$taxonKey)
+
+  results <- lapply(species_list, function(species_left_out) {
+    df_subset  <- data_cube_df$data %>%
+      # Filter out the data for the species to leave out
+      filter(.data$taxonKey != species_left_out)
+
+    # Calculate the indicator for the remaining data
+    data_cube_df_subset <- data_cube_df
+    data_cube_df_subset$data <- df_subset
+    indicator_result <- fun(data_cube_df_subset)$data
+
+    # Merge with the true indicator to enable comparison
+    comparison_result <- indicator_result %>%
+      rename("loo_val" = "diversity_val") %>%
+      left_join(true_indicator, by = temporal_col_name) %>%
+      mutate(diff = .data$diversity_val - .data$loo_val,
+             rel_diff = .data$diff / .data$loo_val,
+             perc_diff = .data$rel_diff * 100)
+
+    # Return the result with the species left out
+    cbind(species_left_out, comparison_result)
+  })
+
+  out_df <- do.call(rbind.data.frame, results) %>%
+    arrange(year, species_left_out)
+
+  return(out_df)
+}
