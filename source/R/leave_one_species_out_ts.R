1afb4444fe6b65e74600fc05152efd99072ddcca
Ward Langeraert <ward.langeraert@inbo.be>
2024-10-24 17:57:51 +0200

    calculate more error measures

--- source/R/leave_one_species_out_ts.R
+++ source/R/leave_one_species_out_ts.R
@@ -21,9 +21,7 @@
 leave_one_species_out_ts <- function(
     data_cube_df,
     fun,
-    temporal_col_name = "year",
-    ...,
-    seed = 123) {
+    temporal_col_name = "year") {
   require("dplyr")
   require("rlang")
 
@@ -47,16 +45,25 @@ leave_one_species_out_ts <- function(
     comparison_result <- indicator_result %>%
       rename("loo_val" = "diversity_val") %>%
       left_join(true_indicator, by = temporal_col_name) %>%
-      mutate(diff = .data$diversity_val - .data$loo_val,
-             rel_diff = .data$diff / .data$loo_val,
-             perc_diff = .data$rel_diff * 100)
+      mutate(
+        error = .data$diversity_val - .data$loo_val,
+        sq_error = error^2,
+        abs_diff = abs(.data$error),
+        rel_diff = .data$abs_diff / .data$loo_val,
+        perc_diff = .data$rel_diff * 100
+      )
 
     # Return the result with the species left out
     cbind(species_left_out, comparison_result)
   })
 
   out_df <- do.call(rbind.data.frame, results) %>%
-    arrange(year, species_left_out)
+    arrange(year, species_left_out) %>%
+    mutate(
+      mse = mean(sq_error),
+      rmse = sqrt(mse),
+      .by = all_of(temporal_col_name)
+    )
 
   return(out_df)
 }
